7670e084d615d567060d7a8cd255df8ecc1f3f4e
Adriano de Oliveira <adriano.98@gmail.com>
2022-06-10 01:18:43 -0300

    Adicionando aulas 07 a 09

---
+++ aula08-ProjetoFinal/public/index.php
@@ -0,0 +1,50 @@
+<?php
+
+include '../vendor/autoload.php';
+
+$database = 'db_store';
+$username = 'root';
+$password = '1234';
+
+$connection = new PDO("mysql:host=localhost;dbname=$database", $username, $password);
+
+var_dump($connection);
+
+// use App\Controller\IndexController;
+// use App\Controller\ProductController;
+// use App\Controller\CategoryController;
+// use App\Controller\ErrorController;
+
+// $url = explode('?', $_SERVER['REQUEST_URI'])[0];
+
+// function createRoute(string $controllerName, string $actionName)
+// {
+//     return [
+//         'controller' => $controllerName,
+//         'action' => $actionName,
+//     ];
+// }
+
+// $routes = [
+//     '/' => createRoute(IndexController::class, 'indexAction'),
+//     '/login' => createRoute(IndexController::class, 'loginAction'),
+//     '/produtos' => createRoute(ProductController::class, 'listAction'),
+//     '/produtos/novo' => createRoute(ProductController::class, 'addAction'),
+//     '/produtos/edit' => createRoute(ProductController::class, 'editAction'),
+//     '/categorias' => createRoute(CategoryController::class, 'listAction'),
+//     '/categorias/novo' => createRoute(CategoryController::class, 'addAction'),
+//     '/categorias/edit' => createRoute(CategoryController::class, 'editAction'),
+
+// ];
+
+// if (false === isset($routes[$url])) {
+//     (new ErrorController())->notFoundAction();
+//     exit;
+// }
+
+// $controllerName = $routes[$url]['controller'];
+// $actionName = $routes[$url]['action'];
+
+// (new $controllerName())->$actionName();
+
+// var_dump($routes[$url]);
